e74ff83ae4adf8948ae6df187a28d959546d5051
Sanghean Lee <francesco419@naver.com>
2022-03-21 17:12:09 +0900

    1st comoit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movie/:id" element={<Detail/>} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,38 @@
+import {useState, useEffect} from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+    const [loading,setLoading] = useState(true);
+  const [movies, setMovie] = useState([]);
+  const getMovies = async()=>{
+    const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year")).json();
+    //const json=await response.json();
+    setMovie(json.data.movies);
+    setLoading(false);
+  }
+  useEffect(()=>{
+    getMovies();
+  },[])
+
+  return (
+    <div>
+      {loading ? (
+          <h1> ...loading</h1>
+          ) : (
+            <div>
+        {movies.map((movie)=>(
+        <Movie 
+          CoverImg={movie.medium_cover_image}
+          id={movie.id}
+          title={movie.title} 
+          genres={movie.genres} 
+          summary={movie.summary}
+        />
+        ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Home;
